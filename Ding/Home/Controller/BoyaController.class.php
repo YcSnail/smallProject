4d5a1893326e51148b541970c0e88d75a687722c
wheel <254232364@qq.com>
2017-07-28 10:16:01 +0800

    修改时间选择器 取值范围  ,更新 工作时间计算方法,

--- Ding/Home/Controller/BoyaController.class.php
+++ Ding/Home/Controller/BoyaController.class.php
@@ -3,78 +3,108 @@ namespace Home\Controller;
 use Think\Controller;
 class BoyaController extends Controller {
     public function index(){
-        $t = '2017-07-26 10';
-        $t2 = '2017-07-28 10';
 
-        $stratTime = $this->changeTime($t);
-        $endTime = $this->changeTime($t2);
+        $stratTime = '2017-07-27 14:00'.':00';
+        $endTime =   '2017-07-29 14:30'.':00';
 
-        //计算之间 共有几天
-        $contHour = $this->contHour($stratTime[0],$endTime[0]);
-
-        //精确计算 多少小时;
-        $startHour = $stratTime[1];
-        $endHour = $endTime[1];
-
-        //判断是不是上午
-        //并计算请假当天距离下班的时间
-        if ( (8 <= $startHour)  && ($startHour <= 12)){
-            $qingTime = 12 - $startHour + 5;
-            echo '上午';
-        }else{
-            echo '下午';
-            $qingTime = 17 - $startHour;
-        }
-        echo $qingTime;
-        echo '<br>';
-
-        //并计算请假结束当天距离上班的时间
-        if ( (8 <= $endHour)  && ($endHour <= 12)){
-            //  10 - 8; 上午2小时
-            //  12 - 8; 上午4小时
-
-            $zhongTime = $endHour - 8;
-            echo '上午';
-        }else{
-
-            echo '下午';
-            $zhongTime = 17 - $endHour + 3;
-        }
-        echo $zhongTime;
-        die();
-
-        var_dump($hour);
+        $time = $this->computationTime($stratTime,$endTime);
+        var_dump($time);
         die();
 
         $this->display();
     }
 
     /**
-     * 获取有多少小时
-     * @param $stattime
-     * @param $endtime
-     * @return bool|float|int
+     *
+     * @param $stratTime
+     * @param $endTime
+     * @return int
      */
-    public function contHour($stattime,$endtime){
-        if (empty($stattime) || empty($endtime)){
-            return false;
+    public function computationTime($stratTime,$endTime){
+
+        // 字符串转数组
+        $stratTimeArr = explode(' ',$stratTime);
+        $endTimeArr =  explode(' ',$endTime);
+
+        // 判断过滤一些特殊情况
+        $this->checkTime($stratTimeArr[1]);
+        $this->checkTime($endTimeArr[1]);
+
+        // 获取请假时的时间戳
+        $stattime = strtotime($stratTime);
+        $endtime = strtotime($endTime);
+
+        if ($endtime <= $stattime){
+            $this->ajaxRes(-1,'结束时间 不能大于 开始时间');
         }
-        $stattime = strtotime($stattime);
-        $endtime = strtotime($endtime);
-        $contDay = ($endtime - $stattime)/ 86400;
-        $contHour = $contDay * 8;
-        return $contHour;
+
+        // 拼接获取 请假的时间戳
+        $StartDayTime = $stratTimeArr[0] .' 17:00:00';
+        $endDayTime = $endTimeArr[0] .' 8:30:00';
+
+        // 获取 请假当天 17:00 的时间戳;
+        $leaveStartTime = strtotime($StartDayTime);
+
+        // 获取 上班当天 8:30 的时间戳
+        $leaveStopTime = strtotime($endDayTime);
+
+        // 计算 请假当日 请假的时间
+        $countStrat = $leaveStartTime - $stattime ;
+
+        $contStartTime = $countStrat/3600;
+
+        if ( $contStartTime > 4.5){
+            $contStartTime  -= 0.5;
+        }
+
+        // 计算 到岗日请假了几个小时
+        $contStopTime = ($endtime - $leaveStopTime)/3600;
+        if ( $contStopTime > 4.5 ){
+            $contStopTime  -= 0.5;
+        }
+
+        // 计算有几天
+        // $stattime $endtime
+        $countDay  = ($endtime - $stattime)/86400;
+
+        $countDay = (int)($countDay);
+        $countDay -= 1;
+
+        $computationTime = ($countDay*8) + $contStopTime + $contStartTime;
+
+        return $computationTime;
     }
 
     /**
-     * string 转 array
      * @param $time
-     * @return array
+     * @return bool
      */
-    public function changeTime($time){
-        $stratTime = explode(' ',$time);
-        return $stratTime;
+    public function checkTime($time){
+        $arr = explode(':',$time);
+
+        if (empty($arr)){
+            $this->ajaxRes(-1,'输入时间有误');
+            return false;
+        }
+
+        if ( ($arr[0] == 8 && $arr[1] == 00 ) || ($arr[0] == 17 && $arr[1] == 30 ) ){
+            $this->ajaxRes(-1,'请选择 有效的工作时间');
+            die();
+        }
+
+        return true;
     }
 
+    /**
+     * ajax 返回数据
+     * @param $code
+     * @param string $msg
+     * @param int $type
+     */
+    public function ajaxRes($code,$msg = '',$type = 0){
+
+        $arr = empty($type) ? $arr = array('code'=>$code,'message'=>$msg) : $code ;
+        die(json_encode($arr));
+    }
 
 }
